5de8b5fcf0602087de377775fe44f87b61d15dee
HATBE2113 <admin@aarongensetter.ch>
2023-10-02 15:55:05 +0200

    add temp text in components ts file

--- frontend/src/app/components/pages/profile-page/profile-page.component.ts
+++ frontend/src/app/components/pages/profile-page/profile-page.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePageComponent implements OnInit {
 
+  bikes = [
+    {
+      name: 'Blue Beast',
+      make: 'Suzuki',
+      model: 'GSR 750',
+      year: '2013'
+    },
+    {
+      name: 'Black Beast',
+      make: 'Yamaha',
+      model: 'XJ6NA',
+      year: '2009'
+    },
+    {
+      name: 'Green Beast',
+      make: 'Kawasaki',
+      model: 'Ninja 300',
+      year: '2013'
+    },
+    {
+      name: 'The Slow Gray',
+      make: 'Yamaha',
+      model: 'MT-125',
+      year: '2021'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
