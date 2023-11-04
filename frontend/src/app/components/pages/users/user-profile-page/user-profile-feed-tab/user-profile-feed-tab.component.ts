eb2e1f0733a40057d6578b4458dffb179a4c223a
HATBE2113 <admin@aarongensetter.ch>
2023-11-04 20:57:07 +0100

    little design changes

--- frontend/src/app/components/pages/users/user-profile-page/user-profile-feed-tab/user-profile-feed-tab.component.ts
+++ frontend/src/app/components/pages/users/user-profile-page/user-profile-feed-tab/user-profile-feed-tab.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { faComment, faHeart, faHeartCrack } from '@fortawesome/free-solid-svg-icons';
+import { Pagination } from 'src/app/models/pagination.model';
 
 @Component({
   selector: 'app-user-profile-feed-tab',
@@ -13,9 +14,21 @@ export class UserProfileFeedTabComponent implements OnInit {
 
   @Input() userId: number | null = null;
 
+  pagination: Pagination = {page: 1, limit: 1, maxPages: 1, maxCount: 1, hasLast: false, hasNext: false};
+
+  isLoading: boolean = false;
+  isReLoading: boolean = false;
+
+  error: string = '';
+  data: any = null;
+
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  onPageSwitch(event: any) {
 
   }
 
